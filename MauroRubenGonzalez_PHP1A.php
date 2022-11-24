3efae3c284ebadd609d10ab4ef89c25e4a47be00
Maben <103900474+GhotMiurgho@users.noreply.github.com>
2022-11-23 20:51:34 -0800

    Add files via upload

---
+++ MauroRubenGonzalez_PHP1A.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+
+ECHO
+
+$plussign="+++++++++++++++++++++++++++++++++++++++".
+
+
+$NAME=
+"Hello classmate, I am MAURO R. GONZALEZ.".
+
+$CLASSES=
+" I am currently taking CIS 64, MATH 110, AND ENGLISH 1A.".
+
+$DATE=
+" TODAY IS THU AUG 17.".
+
+$NOTE=
+" ENJOY YOUR DAY.".
+$plussign="+++++++++++++++++++++++++++++++++++++++"
+
+?>
+</body>
+</html>
